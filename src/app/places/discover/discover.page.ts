72313c3f608c49f23be85ada90e0a8d9c9b95a6a
Mugunthan93 <kskarthick93@gmail.com>
2019-08-22 08:20:56 +0530

    navigation is going to end
    Signed-off-by: Mugunthan93 <kskarthick93@gmail.com>

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { PlacesModel } from '../places.model';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-discover',
@@ -12,11 +13,16 @@ export class DiscoverPage implements OnInit {
   loadedPlaces: PlacesModel[];
 
   constructor(
-    private placesService: PlacesService
+    private placesService: PlacesService,
+    private menuCtrl: MenuController
   ) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+  }
+
+  onOpenMenu() {
+    this.menuCtrl.toggle('side1');
   }
 
 }
--- src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PlacesModel } from '../../places.model';
+import { NavComponent } from '@ionic/core';
+import { NavController } from '@ionic/angular';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-offer-bookings',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfferBookingsPage implements OnInit {
 
-  constructor() { }
+  place: PlacesModel;
+
+  constructor(
+    private route: ActivatedRoute,
+    private navCtrl: NavController,
+    private placeService: PlacesService
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(
+      paramMap => {
+        if (!paramMap.has('placeId')) {
+          this.navCtrl.navigateBack('/places/tabs/offers');
+        }
+        this.place = this.placeService.getPlace(paramMap.get('placeId'));
+      }
+    );
   }
 
 }
